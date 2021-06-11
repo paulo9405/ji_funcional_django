340e1227813e8414dffe55732d4e420e539a5e27
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-06-11 20:29:01 -0300

    'antes_crud

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Pessoa, FichaPessoa
 
-# Register your models here.
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = (
+        'nome', 'telefone', 'email', 'plano_mes', 'plano_tri'
+    )
+
+class FichaPessoaAdmin(admin.ModelAdmin):
+    list_display = (
+        'cliente', 'peso', 'altura', 'imc', 'data_inicio', 'observacoes'
+    )
+
+admin.site.register(Pessoa, PessoaAdmin)
+admin.site.register(FichaPessoa, FichaPessoaAdmin)
+
--- core/models.py
+++ core/models.py
@@ -1,14 +1,43 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 import math
-'''
+
 class Pessoa(models.Model):
-    nome = models.CharField(max_length=100)
-    telefone =
-    email =
-    endereço =
-    plano_mes =
-    plano_tri =
-'''
+    nome = models.CharField(u'Nome', max_length=100)
+    telefone = models.CharField(u'Telefone', max_length=50)
+    email = models.EmailField(u'Email', max_length=100,)
+    endereço = models.CharField(u'Endereço', max_length=100)
+
+    plano_mes = models.BooleanField(
+        u'Plano Mensal R$ 50,00', null=True
+    )
+
+    plano_tri = models.BooleanField(
+        u'Plano Trimestral R$ 130,00', null=True
+    )
+
+    def __str__(self):
+        return self.nome
+
+
+class FichaPessoa(models.Model):
+    cliente = models.ForeignKey(Pessoa, on_delete=CASCADE)
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+    altura= models.DecimalField(max_digits=3, decimal_places=2)
+    imc = models.DecimalField(max_digits=4, decimal_places=2)
+    data_inicio = models.DateField()
+    observacoes = models.TextField()
+
+    def __str__(self):
+        return str(self.cliente) + ' - ' + str(self.peso)
+
+
+
+
+
+
+
+
+
 
 
