8e311a9a016ddc8befb10fd581d41fe238cedfde
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-06-11 18:23:01 -0300

    'salvar'

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+import math
+'''
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    telefone =
+    email =
+    endereço =
+    plano_mes =
+    plano_tri =
+'''
+
+
